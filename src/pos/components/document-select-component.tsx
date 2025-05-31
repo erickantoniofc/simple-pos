f3a0aa4d64fd5f84555ebbef264d342731801456
Erick <erickantoniofc@gmail.com>
2025-05-31 10:44:34 -0600

    document-select on pos functionality added

--- src/pos/components/document-select-component.tsx
+++ src/pos/components/document-select-component.tsx
@@ -1,5 +1,31 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+
+
 export const DocumentSelectComponent = () => {
   return (
-    <div>document-select-component</div>
+    <Select>
+      <SelectTrigger className="w-full">
+        <SelectValue placeholder="Documento.." />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Tipo de documento</SelectLabel>
+          <SelectItem value="apple">Credito fiscal</SelectItem>
+          <SelectItem value="banana">Consumidor final</SelectItem>
+          <SelectItem value="blueberry">Nota de credito</SelectItem>
+          <SelectItem value="grapes">Nota de debito</SelectItem>
+          
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   )
 }
